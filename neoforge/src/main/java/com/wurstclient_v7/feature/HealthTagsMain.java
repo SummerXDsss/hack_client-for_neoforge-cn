a0ce6657d85ba8c0b267ab722862bda0f02edc43
JY C <3104391686@qq.com>
2026-06-06 17:09:06 +0800

    Remove health tag payload handshake

--- neoforge/src/main/java/com/wurstclient_v7/feature/HealthTagsMain.java
+++ neoforge/src/main/java/com/wurstclient_v7/feature/HealthTagsMain.java
@@ -1,13 +1,8 @@
 package com.wurstclient_v7.feature;
 
 import com.wurstclient_v7.config.NeoForgeConfigManager;
-import com.wurstclient_v7.client.HealthTagClientCache;
-import com.wurstclient_v7.net.HealthTagsPayloads;
-import com.wurstclient_v7.server.HealthTagsBroadcaster;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
 @Mod(HealthTagsMain.MODID)
 public final class HealthTagsMain {
@@ -26,14 +21,5 @@ public final class HealthTagsMain {
 	}
 
 	public HealthTagsMain(IEventBus modEventBus) {
-		// Register networking
-		modEventBus.addListener(this::registerNetworking);
-
-		// Register server broadcaster
-		HealthTagsBroadcaster.init();
-	}
-
-	private void registerNetworking(RegisterPayloadHandlersEvent event) {
-		HealthTagsPayloads.register(event, HealthTagClientCache.CLIENT_HANDLER);
 	}
 }
